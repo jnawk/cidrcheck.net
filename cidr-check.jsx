c6b1fb5e900e5d172f9fc2427248ddda2ae9485c
Philip Dowie <philip@jnawk.nz>
2021-02-17 00:16:13 +1300

    improve UI, add address binary for v4

--- cidr-check.jsx
+++ cidr-check.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 const autoBind = require("auto-bind")
-
 const ipaddress = require('ip-address')
+
+import { Container, Row, Col } from "react-bootstrap"
 
 
 class CIDRCheck extends React.Component {
@@ -19,48 +20,69 @@ class CIDRCheck extends React.Component {
     render(){
         const { ipAddress } = this.state
         let details = null
+        let binaryAddressDetails = null
         try {
-          let sa
+          let ipAddressDetails
           try {
-            sa = new ipaddress.Address4(ipAddress)
+            // IPv4 more likely?
+            ipAddressDetails = new ipaddress.Address4(ipAddress)
+
+            // this shit is too bulky in IPv6
+            binaryAddressDetails = (
+              <Row>
+                <Col xs={4} md={3} lg={2}>Address (binary)</Col>
+                <Col xs={1}>
+                  <font color="red">
+                    {ipAddressDetails.mask()}
+                  </font>
+                  <font color="blue">
+                    {ipAddressDetails.getBitsBase2(ipAddressDetails.subnetMask)}
+                  </font>
+                </Col>
+              </Row>
+            )
           } catch {
-            sa = new ipaddress.Address6(ipAddress)
+            ipAddressDetails = new ipaddress.Address6(ipAddress)
           }
 
-          // TODO a TABLE?  yuk!
           details = (
             <>
-              <br />
-              <table>
-              <tbody>
-                <tr>
-                  <th>Address</th>
-                  <td>{sa.addressMinusSuffix}{sa.subnet}</td>
-                </tr>
-                <tr>
-                  <th>Start of Subnet</th>
-                  <td>{sa.startAddress().address}</td>
-                </tr>
-                <tr>
-                  <th>End of Subnet</th>
-                  <td>{sa.endAddress().address}</td>
-                </tr>
-                </tbody>
-              </table>
+              <Row>
+                <Col xs={4} md={3} lg={2}>Address</Col>
+                <Col xs={1}>
+                  {ipAddressDetails.addressMinusSuffix}{ipAddressDetails.subnet}
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={4} md={3} lg={2}>Start of Subnet</Col>
+                <Col xs={1}>{ipAddressDetails.startAddress().address}</Col>
+              </Row>
+              <Row>
+                <Col xs={4} md={3} lg={2}>End of Subnet</Col>
+                <Col xs={1}>{ipAddressDetails.endAddress().address}</Col>
+              </Row>
+              {binaryAddressDetails}
             </>
           )
         } catch {
           // not a valid IPv4 or IPv6 address
         }
         return (
-            <>
-              <label htmlFor="ipAddress">IP Address</label>
-              <input name="ipAddress"
-                value={ipAddress}
-                onChange={event => this.setState({ipAddress: event.target.value})}
-                placeholder="127.0.0.1"/>
-              {details}
-            </>
+          <Container>
+            <Row>
+              <Col xs={4} md={3} lg={2}>
+                <label htmlFor="ipAddress">IP Address</label>
+              </Col>
+              <Col xs={1}>
+                <input name="ipAddress"
+                  value={ipAddress}
+                  onChange={event => this.setState({ipAddress: event.target.value})}
+                  placeholder="127.0.0.1/8"
+                  autoFocus/>
+              </Col>
+            </Row>
+            {details}
+          </Container>
         )
     }
 }
